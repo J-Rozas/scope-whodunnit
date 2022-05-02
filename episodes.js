04b63ef2830adde80f6ef85e4ade3a3e4f74f9ef
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-02 21:39:34 +0100

    Complete ninth episode

--- episodes.js
+++ episodes.js
@@ -145,39 +145,56 @@
 
 
 // Episode 8
-const scenario = {
-  murderer: 'Mrs. Peacock',
-  room: 'Conservatory',
-  weapon: 'Lead Pipe'
-};
+// const scenario = {
+//   murderer: 'Mrs. Peacock',
+//   room: 'Conservatory',
+//   weapon: 'Lead Pipe'
+// };
 
-const changeScenario = function() {
-  scenario.murderer = 'Mrs. Peacock';
-  scenario.room = 'Dining Room';
+// const changeScenario = function() {
+//   scenario.murderer = 'Mrs. Peacock';
+//   scenario.room = 'Dining Room';
 
-  const plotTwist = function(room) {
-    if (scenario.room === room) {
-      scenario.murderer = 'Colonel Mustard';
-    }
+//   const plotTwist = function(room) {
+//     if (scenario.room === room) {
+//       scenario.murderer = 'Colonel Mustard';
+//     }
 
-    const unexpectedOutcome = function(murderer) {
-      if (scenario.murderer === murderer) {
-        scenario.weapon = 'Candle Stick';
-      }
-    }
+//     const unexpectedOutcome = function(murderer) {
+//       if (scenario.murderer === murderer) {
+//         scenario.weapon = 'Candle Stick';
+//       }
+//     }
 
-    unexpectedOutcome('Colonel Mustard');
-  }
+//     unexpectedOutcome('Colonel Mustard');
+//   }
 
-  plotTwist('Dining Room');
-}
+//   plotTwist('Dining Room');
+// }
 
-const declareWeapon = function() {
-  return `The weapon is ${scenario.weapon}.`
-}
+// const declareWeapon = function() {
+//   return `The weapon is ${scenario.weapon}.`
+// }
 
-changeScenario();
-const verdict = declareWeapon();
-console.log(verdict);
+// changeScenario();
+// const verdict = declareWeapon();
+// console.log(verdict);
 
 // My prediction is that the output is going to be The weapon is Candle Stick.
+
+
+// Episode 9
+let murderer = 'Professor Plum';
+
+if (murderer === 'Professor Plum') {
+  let murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// My prediction is that the output is going to be The murderer is Professor Plum
