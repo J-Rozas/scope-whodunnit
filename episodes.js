0633b468504a6618364cf0a1c84b2d9ccb3edf5e
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-02 21:16:55 +0100

    Complete second episode

--- episodes.js
+++ episodes.js
@@ -1,15 +1,33 @@
 // Episode 1
-const scenario = {
-  murderer: 'Miss Scarlet',
-  room: 'Library',
-  weapon: 'Rope'
-};
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
 
-const declareMurderer = function() {
-  return `The murderer is ${scenario.murderer}.`;
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// My prediction is that the output is going to be "The murderer is Miss Scarlet"
+
+
+// Episode 2
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
 }
 
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
 const verdict = declareMurderer();
 console.log(verdict);
 
-// My prediction is that the output is going to be "The murderer is Miss Scarlet"
+// My prediction is that there is going to be an error, because changeMurderer tries to change the value the constant murderer
