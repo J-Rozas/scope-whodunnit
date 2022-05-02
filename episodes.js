1d3af51130200eca4096999a56b0803a0f8242d7
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-02 20:50:12 +0100

    Complete episode 1

---
+++ episodes.js
@@ -0,0 +1,15 @@
+// Episode 1
+const scenario = {
+  murderer: 'Miss Scarlet',
+  room: 'Library',
+  weapon: 'Rope'
+};
+
+const declareMurderer = function() {
+  return `The murderer is ${scenario.murderer}.`;
+}
+
+const verdict = declareMurderer();
+console.log(verdict);
+
+// My prediction is that the output is going to be "The murderer is Miss Scarlet"
